5b5c76c9e7ecdf900a42526a68950bd76f858506
baba_s <shotababa1206@gmail.com>
2019-11-24 19:52:06 +0900

    fix permission

--- Assets/QRCodeReader.cs
+++ Assets/QRCodeReader.cs
@@ -1,38 +1,52 @@
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.UI;
 using ZXing;
 
 public sealed class QRCodeReader : MonoBehaviour
 {
-    public Text     m_text;
-    public RawImage m_rawImage;
+	private const string PERMISSION = Permission.Camera;
 
-    private WebCamTexture m_webCamTexture;
+	public Text     m_text;
+	public RawImage m_rawImage;
 
-    private void Awake()
-    {
-        var width  = Screen.width;
-        var height = Screen.height;
+	private WebCamTexture m_webCamTexture;
 
-        m_webCamTexture = new WebCamTexture( width, height );
-        m_webCamTexture.Play();
+	private void Awake()
+	{
+		Permission.RequestUserPermission( PERMISSION );
+	}
 
-        m_rawImage.texture = m_webCamTexture;
-    }
+	private void Update()
+	{
+		if ( m_webCamTexture == null )
+		{
+			if ( Permission.HasUserAuthorizedPermission( PERMISSION) )
+			{
+				var width  = Screen.width;
+				var height = Screen.height;
 
-    private void Update()
-    {
-        m_text.text = Read( m_webCamTexture );
-    }
+				m_webCamTexture = new WebCamTexture( width, height );
 
-    private static string Read( WebCamTexture texture )
-    {
-        var reader = new BarcodeReader();
-        var rawRGB = texture.GetPixels32();
-        var width  = texture.width;
-        var height = texture.height;
-        var result = reader.Decode( rawRGB, width, height );
+				m_webCamTexture.Play();
 
-        return result != null ? result.Text : string.Empty;
-    }
+				m_rawImage.texture = m_webCamTexture;
+			}
+		}
+		else
+		{
+			m_text.text = Read( m_webCamTexture );
+		}
+	}
+
+	private static string Read( WebCamTexture texture )
+	{
+		var reader = new BarcodeReader();
+		var rawRGB = texture.GetPixels32();
+		var width  = texture.width;
+		var height = texture.height;
+		var result = reader.Decode( rawRGB, width, height );
+
+		return result != null ? result.Text : string.Empty;
+	}
 }
